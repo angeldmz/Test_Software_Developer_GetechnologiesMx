59dfc7a8ec2e5957c129914d8930072701f9d365
angeldmz <139183778+angeldmz@users.noreply.github.com>
2023-07-20 22:00:19 -0600

    Update Factura.java

--- src/Test/Factura.java
+++ src/Test/Factura.java
@@ -1,1 +1,12 @@
+package com.ejemplo.model;
+
+import java.time.LocalDate;
+
+public class Factura {
+    private Long id;
+    private LocalDate fecha;
+    private double monto;
+
+    // Constructor, getters y setters
+}
 
