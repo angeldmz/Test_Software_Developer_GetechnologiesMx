46a046f89a29e270b8dbfcd326d42d45c15a6003
angeldmz <139183778+angeldmz@users.noreply.github.com>
2023-07-20 22:20:09 -0600

    Update Persona.java

--- src/Test/Persona.java
+++ src/Test/Persona.java
@@ -1,4 +1,6 @@
-package Test;
+package com.ejemplo.model;
+
+import java.util.List;
 
 public class Persona {
     private Long id;
@@ -7,5 +9,45 @@ public class Persona {
     private String apellidoMaterno;
     private String identificacion;
 
-    // Constructor, getters y setters
+    // Constructor vacío requerido por JPA
+    public Persona() {
+    }
+    // Constructor con todos los atributos obligatorios excepto apellidoMaterno
+    public Persona(Long id, String nombre, String apellidoPaterno, String identificacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.identificacion = identificacion;
+    }
+    // Getters y setters para todos los atributos
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    public String getIdentificacion() {
+        return identificacion;
+    }
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
 }
