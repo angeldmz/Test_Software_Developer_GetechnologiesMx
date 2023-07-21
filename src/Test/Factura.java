22f2299535e1686e435cf7e951a43ac5ea5edea5
angeldmz <139183778+angeldmz@users.noreply.github.com>
2023-07-20 22:21:11 -0600

    Update Factura.java

--- src/Test/Factura.java
+++ src/Test/Factura.java
@@ -1,5 +1,4 @@
-package Test;
-
+package com.ejemplo.model;
 import java.time.LocalDate;
 
 public class Factura {
@@ -7,6 +6,33 @@ public class Factura {
     private LocalDate fecha;
     private double monto;
 
-    // Constructor, getters y setters
-}
+    // Constructor vacío requerido por JPA
+    public Factura() {
+    }
 
+    // Constructor con todos los atributos
+    public Factura(Long id, LocalDate fecha, double monto) {
+        this.id = id;
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+    // Getters y setters para todos los atributos
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public double getMonto() {
+        return monto;
+    }
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+}
