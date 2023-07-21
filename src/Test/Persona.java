f85a029d8a255c8802e47954c0ed59858b080fa2
angeldmz <139183778+angeldmz@users.noreply.github.com>
2023-07-20 21:59:18 -0600

    Update Persona.java

--- src/Test/Persona.java
+++ src/Test/Persona.java
@@ -1,1 +1,11 @@
+package Test;
 
+public class Persona {
+    private Long id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String identificacion;
+
+    // Constructor, getters y setters
+}
